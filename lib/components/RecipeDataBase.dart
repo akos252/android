e1b4a10adbdbc5b6d59e8bbf1f2f1b3bf3d0ef03
akos252 <akosfekete025@gmail.com>
2024-10-04 19:38:16 +0200

    Akos

---
+++ lib/components/RecipeDataBase.dart
@@ -0,0 +1,55 @@
+List Indian = [
+  {
+    "asset": "assets/mutton.jpeg",
+    "name": "Gongura bárányhús",
+    "items": '250 Gram Bárány hús\n'
+        'Kurkuma por ízlés szerint.\n'
+        'só ízlés szerint\nGyömbér-fokhagyma paszta\n2 evőkanál finomított '
+        'olaj\n'
+        '6 szegfűszeg\n'
+        '3 kardamom\n'
+        '1 rúd fahéj\n'
+        '1/2 '
+        'evőkanál köménymagvak\n'
+        '3 evőkanál felvágott hagyma\n'
+        '1 teáskanál '
+        'győmbér-fokhagyma paszta\n'
+        '100 Gram gongura levél\n'
+        '1 '
+        'evőkanál vörös chili por\n'
+        '1 teáskanál korianderpor'
+        '\n'
+        '1/2 tsp Turmeric powder\n'
+        '10 Curry leaves\n6 vágott zöld chili\n'
+        'Coriander leaves, chopped\n50ml '
+        'Water\n'
+        '1 1/2 tsp Salt\n'
+        'vörös chili & koriandervelél, '
+        'fried\n'
+        'tiszta vaj\n',
+    "folyamat": '1.Főzd meg a húst kurkumával és sóval.'
+        ' and ginger garlic paste, keep aside.\n'
+        '2.Serpenyőben melegítsen olajat.\n'
+        '3.Adjon hozzá szegfűszeget, kardamomot, köménymagot és apróra v\n'
+        '4.Sütögetd a hagymát, amíg átlátszóvá nem válik.\n'
+        '5.Majd adj hozzá gyömbér-fokhagyma pasztát, gongura leveleket, pirospaprikát, korianderport, kurkumát, curry leveleket, felvágott zöld chilit, aprított korianderleveleket, főtt húst, vizet és sót.\n'
+        '6.Jól keverd össze és főzd 5 percig.'
+        '.\n'
+        '7.Díszítsd pirított vörös chilivel, curry levelekkel és tisztított vajjal.\n'
+        '8.Meleg rizzsel '
+        'tálaljuk.'
+  },
+];
+List French = [
+  {
+    "asset": "assets/crepes.jpg",
+    "name": "Narancsos és maracujás kompóttal töltött palacsinta",
+    "items": "8 passiógyümölcs, félbe vágva\n1/3 csésze (80ml) világos agavé szirup vagy juhar szirup\n1 csésze (150g) liszt, szitált\n2 tojás\n300ml tej"
+        "\n50g vaj, olvasztott\n250g creme fraiche vagy tejföl\n1 teáskanál vanília kivonat\n100g mogyoró, pirítva, széttörve",
+    "folyamat": "1. A kompót elkészítéséhez helyezd a narancsszeleteket és a narancs levét egy lábasba. Kanalazd a maracuja belsejét egy szitába, amelyet a lábas fölé helyeztél, és nyomkodd át, hogy kinyerd a levet. A magokat dobd ki. Adj 2 evőkanál agavé szirupot a lábasba, és közepes lángon forrald fel. Szűrőkanállal tedd át a narancsszeleteket egy tálba. Forrald fel a folyadékot, majd közepesen magas lángon párold 5 percig, vagy amíg a felére csökken. Öntsd a narancsra, és tedd félre hűlni.
+    2. A palacsinta elkészítéséhez keverd össze a lisztet és egy csipet sót egy nagy tálban. Csinálj egy mélyedést a közepébe, add hozzá a tojásokat, majd fokozatosan add hozzá a tejet, folyamatosan keverve, amíg sima tésztát nem kapsz – az állagának nagyon híg tejszínre kell hasonlítania (ha szükséges, adj hozzá több tejet). Alternatívaként használhatsz turmixgépet is. Öntsd a tésztát egy kancsóba, fedd le, és hagyd szobahőmérsékleten állni 20 percig.
+    3. Melegítsd elő a sütőt 150°C-ra. Helyezz egy 16 cm-es palacsintasütőt közepes lángra, és kend meg egy kevés olvasztott vajjal. Önts bele annyi tésztát, hogy majdnem befedje a serpenyő alját, majd forgasd meg a serpenyőt, hogy teljesen bevonja. Süsd 1 percig, majd fordítsd meg, és süsd további 30 másodpercig, vagy amíg aranybarna nem lesz. Tedd egy tányérra, és tartsd melegen a sütőben. Ismételd meg a maradék vajjal és tésztával, hogy 12 palacsintát készíts.
+    4. Tedd a crème fraîche-t, a vaníliát és a maradék 2 evőkanál agavé szirupot egy tálba, és keverd össze.
+    5. Tegyél egy kanálnyi crème fraîche-t és 3 narancsszeletet minden palacsinta egynegyedére. Hajtsd félbe a palacsintát, hogy befedd a tölteléket, majd hajtsd félbe még egyszer, hogy egy csomagot kapj. Ismételd meg a többi palacsintával és töltelékkel. Oszd szét a palacsintákat a tányérok között, és a tetejükre tegyél a maradék crème fraîche-ből és kompótból. Szórd meg mogyoróval, majd tálald."
+     }
+];
